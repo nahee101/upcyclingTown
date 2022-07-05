0571448ed3b675da5a0c3a45ff4cefc07815343e
jieun yun <lesson0186@gmail.com>
2022-07-05 16:16:22 +0900

    022.07

---
+++ upcycling/src/components/grade/gradeSlice.js
@@ -0,0 +1,20 @@
+import { createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+    gradeList : [
+        {}
+    ]
+}
+
+const greadeSlice = createSlice({
+    name : 'gradeList',
+    initialState,
+    reducers : {
+        showGrade(state, action) {
+            
+        }
+    }
+})
+
+export const { showGrade } =greadeSlice.actions
+export default greadeSlice.reducer;
